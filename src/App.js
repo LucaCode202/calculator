53a65d4851cc7b5fc3c780274302370fb4c92e98
luca <lucacamillo@live.co.uk>
2023-11-02 15:30:31 +0000

    comit of the calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Wrapper from "./components/Wrapper";
+import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
+import Button from "./components/Button";
+import React, { useState } from "react";
 
-function App() {
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
+const toLocaleString = (num) =>
+  String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+
+const removeSpaces = (num) => num.toString().replace(/\s/g, "");
+
+const App = () => {
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Screen value={0} />
+      <ButtonBox calc={calc} setCalc={setCalc} btnValues={btnValues} calcNum={calc.num} calcRes={calc.res} calcArray={btnValues}>
+        {
+          btnValues.flat().map((btn, i) => {
+            return (
+              <Button
+                key={i}
+                className={btn === "=" ? "equals" : ""}
+                value={btn}
+                onClick={() => {
+                  console.log(`${btn} clicked!`);
+                }}
+              />
+            );
+          })
+        }
+      </ButtonBox>
+    </Wrapper>
   );
-}
+};
 
 export default App;
---
+++ src/components/ButtonBox.js
@@ -0,0 +1,150 @@
+import "./ButtonBox.css";
+import React, { useState } from "react";
+import Wrapper from "./Wrapper";
+import Screen from "./Screen";
+import Button from "./Button";
+
+
+// ...
+
+const ButtonBox = ({calc, setCalc, btnValues, calcNum, calcRes, calcArray}) => {
+  // ...
+  const numClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    if (calc.num.length < 16) {
+      setCalc({
+        ...calc,
+        num:
+          calc.num === 0 && value === "0"
+            ? "0"
+            : calc.num % 1 === 0
+            ? Number(calc.num + value)
+            : calc.num + value,
+        res: !calc.sign ? 0 : calc.res,
+      });
+    }
+  };
+
+  const commaClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+  
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+    });
+  };
+
+  const signClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+  
+    setCalc({
+      ...calc,
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0,
+    });
+  };
+
+  const equalsClickHandler = () => {
+    if (calc.sign && calc.num) {
+      const math = (a, b, sign) =>
+        sign === "+"
+          ? a + b
+          : sign === "-"
+          ? a - b
+          : sign === "X"
+          ? a * b
+          : a / b;
+  
+      setCalc({
+        ...calc,
+        res:
+          calc.num === "0" && calc.sign === "/"
+            ? "Can't divide with 0"
+            : math(Number(calc.res), Number(calc.num), calc.sign),
+        sign: "",
+        num: 0,
+      });
+    }
+  };
+
+  const invertClickHandler = () => {
+    setCalc({
+      ...calc,
+      num: calc.num ? calc.num * -1 : 0,
+      res: calc.res ? calc.res * -1 : 0,
+      sign: "",
+    });
+  };
+
+  const percentClickHandler = () => {
+    let num = calc.num ? parseFloat(calc.num) : 0;
+    let res = calc.res ? parseFloat(calc.res) : 0;
+  
+    setCalc({
+      ...calc,
+      num: (num /= Math.pow(100, 1)),
+      res: (res /= Math.pow(100, 1)),
+      sign: "",
+    });
+  };
+
+  const resetClickHandler = () => {
+    setCalc({
+      ...calc,
+      sign: "",
+      num: 0,
+      res: 0,
+    });
+  };
+
+   btnValues = [
+    ["C", "+-", "%", "/"],
+    [7, 8, 9, "X"],
+    [4, 5, 6, "-"],
+    [1, 2, 3, "+"],
+    [0, ".", "="],
+  ];
+
+  return (
+    <Wrapper>
+    <Screen value={calcNum ? calcNum : calcRes}/>
+    <ButtonBox>
+      {btnValues.flat().map((btn, i) => {
+        return (
+          <Button
+            key={i}
+            className={btn === "=" ? "equals" : ""}
+            value={btn}
+            onClick={
+              btn === "C"
+                ? resetClickHandler
+                : btn === "+-"
+                ? invertClickHandler
+                : btn === "%"
+                ? percentClickHandler
+                : btn === "="
+                ? equalsClickHandler
+                : btn === "/" || btn === "X" || btn === "-" || btn === "+"
+                ? signClickHandler
+                : btn === "."
+                ? commaClickHandler
+                : numClickHandler
+            }
+          />
+        );
+      })};
+    </ButtonBox>
+  </Wrapper>
+  );
+};
+
+// const ButtonBox = ({ children }) => {
+//   return <div className="buttonBox">{children}</div>;
+// };
+
+export default ButtonBox;
